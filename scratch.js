065d18923c01a051710f9960983e1a65842b22cc
Taylor Cooke <47223694+tlcpack@users.noreply.github.com>
2021-04-19 15:21:04 -0400

    Update scratch.js
    added http client class

--- scratch.js
+++ scratch.js
@@ -1,37 +1,27 @@
-function weave(a,b) {
-	let final = '';
-  const stringA = a.toString();
-  const stringB = b.toString();
-  
-  for (let i = 0; i < stringA.length || i < stringB.length; i++) {
-  	if (stringA[i]) {
-  		final += stringA[i];
-    }
-    if (stringB[i]) {
-      final += stringB[i]; 
-    }
+class Provider {
+  constructor(client, baseUrl, options = {}) {
+    this.client = client;
+    this.baseUrl = baseUrl;
+    this.options = options;
   }
-  return final;
+
+  async fetchResponse(endpoint, options = {}) {
+    const response = await fetch(this.baseUrl + endpoint, { 
+      ...options
+    });
+
+    if(!response.ok) throw Error(response.statusText);
+
+    return response.json();
+  }
+
+  getRates(endpoint, options = {}) {
+    return this.fetchResponse(
+      endpoint, {
+      ...options
+    });
+  }
 }
 
-// console.log(weave("abcghgfhdlds", "123"));
-
-
-let users = {
-  1: {
-    id: '1',
-    firstName: 'Robin',
-    lastName: 'Wieruch',
-    isDeveloper: true,
-  },
-  2: {
-    id: '2',
-    firstName: 'Dave',
-    lastName: 'Davddis',
-    isDeveloper: false,
-  },
-};
-
-const getUsers = () => Object.values(users);
-const result = getUsers();
-console.log(result);
+const clientOptions = {};
+let newClient = new Provider('ABC Needs Money', 'http://www.abc.com/', clientOptions);
